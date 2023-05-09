530d56b0211e2f2678698e9f4d77c18211a3a1e7
root <root@DESKTOP-CSPRQ1M>
2023-05-09 18:02:10 +0530

    work: changes

---
+++ array_problems/objectProblems.js
@@ -0,0 +1,7 @@
+class Solutions
+{
+    sortObj(arr, n)
+    {
+
+    }
+}
---
+++ string_problems/longest_common_prefix.js
@@ -0,0 +1,11 @@
+class Solution
+{
+    longest_common_prefix(str)
+    {
+         return str + " Here"
+    }
+}
+const solution = new Solution()
+const str = "Nothing"
+let result = solution.longest_common_prefix(str)
+console.log(result)
