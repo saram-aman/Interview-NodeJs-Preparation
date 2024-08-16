5347cd62cc4d01127355f95b0c25662afd0495ba
saram-aman <saram.chaudharyaman2408@gmail.com>
2024-08-16 17:40:43 +0530

    work in progress

--- array_problems/removeRepeated.js
+++ array_problems/removeRepeated.js
@@ -15,11 +15,31 @@ class ArrayProblems {
         }, [])
         return arr.filter(i => frequency[i] === 1)
     }
+
+    async func_three(arr) {
+        let frequency = []
+        let result = []
+        for (let i = 0; i < arr.length; i++) {
+            frequency[arr[i]] = (frequency[arr[i]] || 0) + 1
+        }
+        for (let i = 0; i < arr.length; i++) {
+            if (frequency[arr[i]] === 1) result.push(arr[i])
+        }
+        return result
+    }
 }
 const array_problems = new ArrayProblems()
-Promise.all([array_problems.func_one([2,2,3,2]), array_problems.func_two([0,1,0,1,0,1,99])])
-    .then(([func_one, func_two]) => {
-        console.log(func_one, func_two)
+Promise.all([
+         array_problems.func_one([2,2,3,2]),
+         array_problems.func_two([0,1,0,1,0,1,99]),
+         array_problems.func_three([3,6,2,3,5,8,6])
+     ])
+    .then(([
+        func_one,
+        func_two,
+        func_three
+    ]) => {
+        console.log(func_one, func_two, func_three)
     })
     .catch((err) => {
         console.error('An error accrued in the code:', err)
