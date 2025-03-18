9cc33768e07b37847a5237cf43049efa5ba02534
Aman Chaudhary <aman.chaudhary@bizsupportc.com>
2025-03-18 13:36:13 +0530

    Added react questions!

---
+++ React/Apps/Calculator.tsx
@@ -0,0 +1,119 @@
+import React, { Component } from 'react';
+
+interface CalculatorState {
+    displayValue: string;
+    operator: string | null;
+    previousValue: string | null;
+}
+
+class Calculator extends Component {
+    state: CalculatorState = {
+        displayValue: '0',
+        operator: null,
+        previousValue: null,
+    };
+    constructor(props: {}) {
+        super(props);
+        this.state = {
+            displayValue: '0',
+            operator: null,
+            previousValue: null,
+        };
+    }
+
+    inputDigit(digit: string) {
+        const { displayValue } = this.state;
+        this.setState({
+            displayValue: displayValue === '0' ? digit : displayValue + digit,
+        });
+    }
+
+    inputDot() {
+        const { displayValue } = this.state;
+        if (!displayValue.includes('.')) {
+            this.setState({
+                displayValue: displayValue + '.',
+            });
+        }
+    }
+
+    clearDisplay() {
+        this.setState({
+            displayValue: '0',
+        });
+    }
+
+    handleOperator(nextOperator: string) {
+        const { displayValue, operator, previousValue } = this.state;
+        const inputValue = parseFloat(displayValue);
+
+        if (previousValue == null) {
+            this.setState({
+                previousValue: displayValue,
+                displayValue: ''
+            });
+        } else if (operator) {
+            const currentValue = previousValue ? parseFloat(previousValue) : 0;
+            const newValue = this.performOperation(currentValue, inputValue, operator);
+
+            this.setState({
+                displayValue: String(newValue),
+                previousValue: String(newValue),
+            });
+        }
+
+        this.setState({
+            operator: nextOperator,
+            displayValue: '0',
+        });
+    }
+
+    performOperation(left: number, right: number, operator: string) {
+        switch (operator) {
+            case '+':
+                return left + right;
+            case '-':
+                return left - right;
+            case '*':
+                return left * right;
+            case '/':
+                return left / right;
+            default:
+                return right;
+        }
+    }
+
+    render() {
+        const { displayValue } = this.state;
+
+        return (
+            <div className="calculator">
+                <div className="calculator-display">{displayValue}</div>
+                <div className="calculator-keypad">
+                    <div className="input-keys">
+                        <div className="function-keys">
+                            <button onClick={() => this.clearDisplay()}>AC</button>
+                        </div>
+                        <div className="digit-keys">
+                            {[...Array(10).keys()].map((digit) => (
+                                <button key={digit} onClick={() => this.inputDigit(String(digit))}>
+                                    {digit}
+                                </button>
+                            ))}
+                            <button onClick={() => this.inputDot()}>.</button>
+                        </div>
+                    </div>
+                    <div className="operator-keys">
+                        <button onClick={() => this.handleOperator('+')}>+</button>
+                        <button onClick={() => this.handleOperator('-')}>-</button>
+                        <button onClick={() => this.handleOperator('*')}>*</button>
+                        <button onClick={() => this.handleOperator('/')}>/</button>
+                        <button onClick={() => this.handleOperator('=')}>=</button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Calculator;
---
+++ React/Apps/TicTacToe.tsx
@@ -0,0 +1,121 @@
+import React, { Component } from 'react';
+
+type SquareValue = 'X' | 'O' | null;
+
+interface SquareProps {
+    value: SquareValue;
+    onClick: () => void;
+}
+
+function Square(props: SquareProps) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
+}
+
+interface BoardState {
+    squares: SquareValue[];
+    xIsNext: boolean;
+}
+
+class Board extends React.Component<{}, BoardState> {
+    state: BoardState;
+    constructor(props: {}) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+            xIsNext: true,
+        };
+    }
+
+    handleClick(i: number) {
+        const squares = this.state.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
+    }
+    setState(arg0: { squares: SquareValue[]; xIsNext: boolean; }) {
+        throw new Error('Method not implemented.');
+    }
+
+    renderSquare(i: number) {
+        return (
+            <Square
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}
+            />
+        );
+    }
+
+    render() {
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
+
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+}
+
+class TicTacToe extends React.Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+            </div>
+        );
+    }
+}
+
+function calculateWinner(squares: SquareValue[]) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
+
+export default TicTacToe;
