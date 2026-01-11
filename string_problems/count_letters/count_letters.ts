d013917e491bc8cb9d32156d2f5ec1710729f3de
Aman Chaudhary <saram.chaudharyaman2408@gmail.com>
2026-01-12 01:09:17 +0530

    chnages

--- string_problems/count_letters/count_letters.ts
+++ string_problems/count_letters/count_letters.ts
@@ -1,14 +1,17 @@
 class UniqueStringProblems {
-    async count_letters(str: string): Promise <{[key: string]: number}> {
+    private str: string;
+    constructor(str: string) {
+        this.str = str;
+    }
+    public async count_letters(): Promise <{[key: string]: number}> {
         const countAlphabets: {[key: string]: number} = {}
-        for (let char of str) {
-            if(/[a-z]/.test(char)) countAlphabets[char] = (countAlphabets[char] || 0) + 0
-        
+        for (let char of this.str) {
+            if(/[a-z]/.test(char)) countAlphabets[char] = (countAlphabets[char] || 0) + 1
         }
         return countAlphabets
     }
 }
-const uniqueStringProblems = new UniqueStringProblems()
-string_problems.count_letters("command is used for compiling, building, and managing Go programs, not directly executing individual files.")
-    .then((count_alpha) => console.log(count_alpha))
-    .catch((err) => console.error(err))
+(async () => {
+    const uniqueStringProblems = new UniqueStringProblems("abcdefghijklmnopqrstuvwxyz");
+    console.log(await uniqueStringProblems.count_letters());
+})();
